51de1d0fefd4e9fa9b0e9f221a28e4c846b6992a
JUAN MARTINEZ <163062478+juanmartinezjorcin@users.noreply.github.com>
2025-06-04 19:23:39 -0300

    carpetas

--- my-app/componentsOriginal/Collapsible.tsx
+++ my-app/componentsOriginal/Collapsible.tsx
@@ -1,9 +1,9 @@
 import { PropsWithChildren, useState } from 'react';
 import { StyleSheet, TouchableOpacity } from 'react-native';
 
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
-import { IconSymbol } from '@/components/ui/IconSymbol';
+import { ThemedText } from '@/componentsOriginal/ThemedText';
+import { ThemedView } from '@/componentsOriginal/ThemedView';
+import { IconSymbol } from '@/componentsOriginal/ui/IconSymbol';
 import { Colors } from '@/constants/Colors';
 import { useColorScheme } from '@/hooks/useColorScheme';
 
